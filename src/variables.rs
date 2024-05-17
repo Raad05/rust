e488c45572f5b1943f276cd70ce5978b0ef2359d
Raad05 <ashabulraad@gmail.com>
2024-05-17 06:31:51 +0600

    string variables

--- src/variables.rs
+++ src/variables.rs
@@ -1,4 +1,5 @@
 pub fn variables() {
+    // integar variables
     let x: i32 = 5;
 
     // variable scopes
@@ -11,4 +12,9 @@ pub fn variables() {
 
     // checks if they are equal
     assert_eq!(x, 5);
+
+    // string variables
+    let my_string: &str = "Rust";
+
+    println!("I am {}!", my_string);
 }
